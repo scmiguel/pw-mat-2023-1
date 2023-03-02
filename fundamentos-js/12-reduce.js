d55b09d8be432a7316a18fca9770b45de55528eb
scmiguel <70957634+scmiguel@users.noreply.github.com>
2023-03-02 12:03:53 +0000

    02-03

---
+++ fundamentos-js/12-reduce.js
@@ -0,0 +1,8 @@
+const num = [2, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 29]
+const nomes = []
+
+let soma = num.reduce((acumulador, elementos) => acumulador + elementos)
+
+let lista = nome.reduce((acumulador, elementos) => acumulador.toUpperCase() + ', ' + elementos.toUpperCase())
+
+console.log({some, lista})
