8c5a19daae7a33fd578ac8f2af8c5b97d36c4a3e
scmiguel <70957634+scmiguel@users.noreply.github.com>
2023-05-25 12:15:35 +0000

    25-05

--- karangos/src/pages/CustomersList.jsx
+++ karangos/src/pages/CustomersList.jsx
@@ -1,91 +1,136 @@
-import React from "react";
-import { Typography } from "@mui/material";
-import Box from '@mui/material/Box';
+import React from 'react'
+import Typography from '@mui/material/Typography'
+import Paper from '@mui/material/Paper';
 import { DataGrid } from '@mui/x-data-grid';
+import { format } from 'date-fns'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import IconButton from '@mui/material/IconButton'
 
+export default function CustomersList() {
 
-export default function CustomersList(){
-    const [state, setState] = React.useState({customers: {}})
+  const [state, setState] = React.useState({
+    customers: {}
+  })
 
-    const {customers} = state
-    
-    React.useEffect(()=>{loadData()},[])
+  // Desestruturando as variáveis de estado
+  const {
+    customers
+  } = state
 
-    async function loadData(){
-        try {
-            const result = await fetch('https://api.faustocintra.com.br/customers')
-            if(result.ok) setState({...state, customers: await result.json()})
-            else throw new Error(`[HTTP ${result.status}] ${result.statusText}`)
-        }
-        catch(error){console.error(error)}
+  // Este useEffect() será executado apenas uma vez, durante o
+  // carregamento da página
+  React.useEffect(() => {
+    loadData()    // Carrega dos dados do back-end
+  }, [])
+
+  async function loadData() {
+    try {
+      const result = await fetch('https://api.faustocintra.com.br/customers')
+
+      // Requisição OK
+      if(result.ok) setState({...state, customers: await result.json()})
+      
+      // Requisição com erro
+      else throw new Error(`[HTTP ${result.status}] ${result.statusText}`)
+
     }
+    catch(error) {
+      // Exibimos o erro no console
+      console.error(error)
+    }
+  }
 
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 90 },
-        {
-          field: 'firstName',
-          headerName: 'First name',
-          width: 150,
-          editable: true,
-        },
-        {
-          field: 'lastName',
-          headerName: 'Last name',
-          width: 150,
-          editable: true,
-        },
-        {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 110,
-          editable: true,
-        },
-        {
-          field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-        },
-      ];
-      
-      const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-      ];
-      
-    return(
-        <>
-            <Typography variant='h1'>Lista de Clientes</Typography>
-            <Box sx={{ height: 400, width: '100%' }}>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    initialState={{
-                    pagination: {
-                        paginationModel: {
-                        pageSize: 5,
-                        },
-                    },
-                    }}
-                    pageSizeOptions={[5]}
-                    checkboxSelection
-                    disableRowSelectionOnClick
-                />
-            </Box>
-            <div>
-                {JSON.stringify(customers)}
-            </div>
-        </>
-    )
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'name',
+      headerName: 'Nome',
+      width: 300
+    },
+    {
+      field: 'ident_document',
+      headerName: 'CPF',
+      align: 'center',
+      headerAlign: 'center',
+      width: 150
+    },
+    {
+      field: 'birth_date',
+      headerName: 'Data nasc.',
+      align: 'center',
+      headerAlign: 'center',
+      width: 100,
+      valueFormatter: params => {
+        if(params.value) return format(new Date(params.value), 'dd/MM/yyyy')
+        else return ''
+      }
+    },
+    {
+      field: 'city',
+      headerName: 'Município/UF',
+      width: 300,
+      // Colocando dois campos na mesma célula
+      valueGetter: params => params.row.city + '/' + params.row.uf
+    },
+    {
+      field: 'phone',
+      headerName: 'Celular',
+      align: 'center',
+      headerAlign: 'center',
+      width: 150
+    },
+    {
+      field: 'email',
+      headerName: 'E-mail',
+      width: 200
+    },
+    {
+      field: 'edit',
+      headerName: 'Editar',
+      headerAlign: 'center',
+      align: 'center',
+      width: 90,
+      renderCell: params => 
+        <IconButton aria-label="Editar">
+          <EditIcon />
+        </IconButton>
+    },
+    {
+      field: 'delete',
+      headerName: 'Excluir',
+      headerAlign: 'center',
+      align: 'center',
+      width: 90,
+      renderCell: params =>
+        <IconButton aria-label="Excluir">
+          <DeleteForeverIcon color="error" />
+        </IconButton>
+    }
+  ];
+  
+  return (
+    <>
+      <Typography variant="h1" sx={{ mb: '50px' }}>
+        Listagem de clientes
+      </Typography>
+
+      <Paper elevation={4} sx={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={customers}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Paper>
+    </>
+  )
 }
